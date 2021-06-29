9bb98c4a493ca99759f3398735a3421318a95eaa
Nguyen Lien <nguyen.k.lien98@gmail.com>
2021-06-29 10:00:00 +0700

    finished asignment 8

---
+++ demo.js
@@ -0,0 +1,136 @@
+// console.log("Area of a circle")
+// var pi = 3.14
+// console.log(typeof pi)
+// var radius = 7
+// console.log(typeof radius)
+// var area = pi * radius ** 2
+// var str1 = ("The area of a circle given the radius ")
+// var str2 = radius;
+// var str3 = " (m) is "
+// var str4 = area
+// var str5 = " (m2)"
+// var res = str1.concat(str2, str3, str4, str5);
+// console.log(res) 
+
+// console.log('The area of a circle given the radius ' + radius + ' (m) is ' + area + ' (m2)')
+
+// console.log(34==10 + 24)
+// console.log("1024"==="10" + "24")
+// console.log("Hello 2021"==="Hello" + " " + 2021)
+// console.log(7===1 + 2 * 3)
+// console.log(16==(1 + 3) ** 2)
+// console.log(1/0)
+// console.log(0==6 % 2)
+// console.log(1.5==5.5 % 2)
+// console.log(123===Number("123"))
+// console.log("string"===typeof(1 + ""))
+// console.log("break")
+// console.log(5 == "5")
+// console.log(5 === "5")
+// console.log(8 != 8.0)
+// console.log(8 !== 8.0)
+// console.log("true" === true)
+// console.log(4 <= 4.0)
+// console.log(7 >= 7)
+// console.log(true && true)
+// console.log(true && false)
+// console.log(true || true)
+// console.log(false || true)
+// console.log(!true)
+// console.log(!false)
+// console.log(false && (true || true))
+// console.log(false && true || true)
+
+// var x = 1
+// if (x==0) {
+//     console.log(x,' is zero');
+// } if (x>0) {
+//     console.log('${x} is a positive number');
+// }if (x<0) {
+//     console.log(x,' is a negative number');
+// }
+
+// var a = 1
+// var b = 2
+// var c = 3
+// if (a<b && b<c) {
+//     console.log (`${a} is the smallest number and ${c} is the biggest number among ${a}, ${b}, ${c}`)
+// }
+// if (b<a && a<c) {
+//     console.log (`${b} is the smallest number and ${c} is the biggest number among ${a}, ${b}, ${c}`)
+// }
+// if (c<a && a<b) {
+//     console.log (`${c} is the smallest number and ${b} is the biggest number among ${a}, ${b}, ${c}`)
+// }
+// if (a<c&&c<b) {
+//     console.log (`${a} is the smallest number and ${b} is the biggest number among ${a}, ${b}, ${c}`)
+// }
+// if (b<c && c<a) {
+//     console.log (`${b} is the smallest number and ${a} is the biggest number among ${a}, ${b}, ${c}`)
+// }
+// if (c<b&&b<a) {
+//     console.log (`${c} is the smallest number and ${a} is the biggest number among ${a}, ${b}, ${c}`)
+// }
+
+// var a = 1
+// var b = 5
+// for (a;a<=b;a++) {
+//   console.log(a)
+
+// }
+// var x = 1
+// var y = 5
+
+// z = 0
+// for (var w=x;w<=y;w++) {
+//   var z = z + w
+  
+// }
+// console.log(`Sum of numbers from ${x} to ${y} is: ${z}`)
+
+// var a = 2021
+// var b = a
+// var c = 0
+// for (var x=1;x<=4;x++) {
+//   var d= b%10
+//   var c= c + d
+//   b=(b-d)/10
+// }
+// console.log(`Sum of digits of ${a} is ${c}`)
+
+// function kimfunction() {
+//     console.log("Wow, it worked!")
+// }
+// kimfunction()
+// function kimfavmovie() {
+//     return("My favorite serie is Hajime no Ippo")
+// }
+// console.log(kimfavmovie())
+
+function kim(Nguyen){
+    return(`Kim`)
+}
+console.log(kim())
+
+// var x = 1
+// function number(x) {
+// if (x==0) {
+//     console.log(x," is zero");
+// } if (x>0) {
+//     console.log(x,"is a positive number");
+// }if (x<0) {
+//     console.log(x," is a negative number");
+// }
+// }
+// number(x)
+
+var a = 111
+var b = 999
+let temp
+temp = a
+a = b
+b = temp
+console.log(`values of a and b after swapping is a=${a} and b=${b}`)
+
+// console.log(Math.floor(Math.random() * 2)); 
+// console.log(Math.floor(Math.random() * 11)); 
